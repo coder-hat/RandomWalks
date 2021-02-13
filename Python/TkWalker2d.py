71573c4828142f5d7e927526414d600985a9a548
coder-hat <kevindjang@gmail.com>
2021-02-13 12:29:24 -0800

    Python 2d walker  and display. Initial code: it works, but it's very basic functionality.

---
+++ Python/TkWalker2d.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+import random
+import time
+
+from Walker2d import Walker2d
+
+GRID_COLS = 250
+GRID_ROWS = 250
+GRID_ALL_START_LOCATION = (GRID_COLS // 2, GRID_ROWS // 2)
+
+NUMBER_OF_PARTICLES = 1024
+CELL_DISPLAY_SIZE = 3
+
+class TkWalker2d:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.color = color
+        self.walker = Walker2d(NUMBER_OF_PARTICLES, GRID_ALL_START_LOCATION)
+
+    def draw(self):
+        self.walker.do_step()
+        self.canvas.delete("all")
+        for location in self.walker.location_counts.keys():
+            x, y = location  # grid coordinates
+            x *= CELL_DISPLAY_SIZE  # display coordinates
+            y *= CELL_DISPLAY_SIZE
+            self.canvas.create_rectangle(x, y, x+CELL_DISPLAY_SIZE, y+CELL_DISPLAY_SIZE, fill=self.color, outline=self.color)
+        self.canvas.after(1, self.draw)  # (time_delay, method_to_execute)
+
+
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    root.title("Diffusion Simulated by Random Walk")
+    root.resizable(0,0)
+    root.wm_attributes("-topmost", 1)
+
+    canvas = tk.Canvas(root, width=GRID_COLS*CELL_DISPLAY_SIZE, height=GRID_ROWS*CELL_DISPLAY_SIZE, bd=0, highlightthickness=0)
+    canvas.pack()
+
+    walkDisplay = TkWalker2d(canvas, "red")
+    walkDisplay.draw()  # Changed per Bryan Oakley's comment (comment from original example in Stack Overflow)
+    tk.mainloop()
+
+
+#----- NOTES
+
+# 1. Tkinter's canvas doesn't have a straightforward "draw a pixel" function.
+# But, we can "trick" canvas into creating a 1-pixel "line" to use as a pixel.
+# See:
+# https://stackoverflow.com/questions/30168896/tkinter-draw-one-pixel
+# self.canvas.create_line(x, y, x + 1, y)
+
+# 2. TkWalker2d class and usage demonstrates tkinter after() method
+# Derived from code by:
+# https://stackoverflow.com/users/926143/7stud
+# posted in discusssion:
+# https://stackoverflow.com/questions/29158220/tkinter-understanding-mainloop
---
+++ Python/Walker2d.py
@@ -0,0 +1,22 @@
+from collections import Counter
+import random
+
+
+class Walker2d:
+    def __init__(self, number_of_particles, all_start_location, move=None, handle_boundary=None):
+        self.location_counts = Counter()
+        self.location_counts[all_start_location] = number_of_particles
+        self.move = move
+        if self.move is None:
+            self.move = lambda location: tuple([i + random.randint(-1, 1) for i in location])
+        if handle_boundary is None:
+            self.handle_boundary = lambda location: location
+
+    def do_step(self):
+        new_counts = Counter()
+        for location, count in self.location_counts.items():
+            for i in range(count):
+                new_location = self.handle_boundary(self.move(location))
+                if new_location is not None:
+                    new_counts[new_location] += 1
+        self.location_counts = new_counts
