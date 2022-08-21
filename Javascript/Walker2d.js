db41f5ecd7cd1d09d991b65eb82745c9d99f0beb
coder-hat <kevindjang@gmail.com>
2022-08-21 13:44:54 -0700

    Adds reference comments to Walker2d.js

--- Javascript/Walker2d.js
+++ Javascript/Walker2d.js
@@ -41,8 +41,25 @@ function doStep(curLocations) {
 // Can try out code on online with:
 // https://www.onlinegdb.com/online_javascript_rhino_interpreter
 
+// Destructuring Assignment
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+
 // "Create a Proper Game Loop"
 // https://spicyyoghurt.com/tutorials/html5-javascript-game-development/create-a-proper-game-loop-with-requestanimationframe
 
 // Create Conway's Game of Life in Javascript
 // https://spicyyoghurt.com/tutorials/javascript/conways-game-of-life-canvas
+
+// HTML Button onclick - JavaScript Click Event Tutorial
+// https://www.freecodecamp.org/news/html-button-onclick-javascript-click-event-tutorial/
+
+// Create a Button in Javascript
+// https://herewecode.io/blog/create-button-javascript/
+
+// Add a button to an HTML page using Javascript
+// https://stackoverflow.com/questions/6123136/add-button-to-an-html-page-using-javascript
+
+// Starting and Stopping Animation Frames
+// Stack Overflow:
+// "How to stop a requestAnimationFrame recursion/loop?"
+// https://stackoverflow.com/questions/10735922/how-to-stop-a-requestanimationframe-recursion-loop
